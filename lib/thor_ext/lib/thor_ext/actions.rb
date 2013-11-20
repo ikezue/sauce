113e4dd2d126a869f6ea68a0ac19ffb7d36cfc20
Chris Enedah <ikezue@gmail.com>
2013-11-19 23:42:19 -0800

    Add replace_file/replace_in_file Thor actions

--- lib/thor_ext/lib/thor_ext/actions.rb
+++ lib/thor_ext/lib/thor_ext/actions.rb
@@ -15,5 +15,23 @@ class Thor
       copy_file source, destination
       remove_file source
     end
+
+    def replace_file(file, options = {})
+      source = options[:copy] || options[:template]
+      return if source.nil?
+
+      remove_file file
+
+      options[:copy] ? copy_file(source, file) : template(source, file)
+    end
+
+    def replace_in_file(relative_path, find, replace)
+      path = File.join(destination_root, relative_path)
+      contents = IO.read(path)
+      unless contents.gsub!(find, replace)
+        raise "#{find.inspect} not found in #{relative_path}"
+      end
+      File.open(path, "w") { |file| file.write(contents) }
+    end
   end
 end
