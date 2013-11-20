aee151cf25fbe09fc69eb196f5a9e54f6df72292
Chris Enedah <ikezue@gmail.com>
2013-11-19 17:20:08 -0800

    Reset localhost port number after Rails tests

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,10 +1,12 @@
 require 'sauce'
 
-# Rewind meteor port number after meteor feature tests
-Around('@meteor') do |scenario, block|
+# Rewind meteor port number after meteor/rails feature tests
+Around('@meteor, @rails') do |scenario, block|
   meteor_port = Sauce::Settings.ports.meteor
+  rails_port = Sauce::Settings.ports.rails
   block.call
   Sauce::Settings['ports']['meteor'] = meteor_port
+  Sauce::Settings['ports']['rails'] = rails_port
   Sauce::Settings.save!
 end
 
