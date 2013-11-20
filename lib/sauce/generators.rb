1866b5d4ef3b09f8316e61076adc3251f32d6572
Chris Enedah <ikezue@gmail.com>
2013-11-20 05:18:41 -0800

    Correct missing `require` error

--- lib/sauce/generators.rb
+++ lib/sauce/generators.rb
@@ -3,3 +3,7 @@ require 'sauce/generators/generator'
 require 'sauce/generators/clojure_script/app_generator'
 
 require 'sauce/generators/meteor/app_generator'
+
+require 'sauce/generators/rails/app/generator'
+require 'sauce/generators/rails/app/builder'
+require 'sauce/generators/rails/app_generator'
