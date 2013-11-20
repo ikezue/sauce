3af10775afb0ee57071d11b53b1c6dc46b5b5fe9
Chris Enedah <ikezue@gmail.com>
2013-11-19 22:27:56 -0800

    Customize Rails app README

--- lib/sauce/generators/rails/app/builder.rb
+++ lib/sauce/generators/rails/app/builder.rb
@@ -6,11 +6,17 @@ module Sauce
     module Rails
       module App
         class Builder < ::Rails::AppBuilder
+          # Any method defined in this class will override a method of the same
+          # name in Rails::AppBuilder (http://bit.ly/I2wipm).
           include Helpers::TemplateVariables
 
           def initialize(generator)
             super
             copy_template_variables_to_instance_variables
+          end
+
+          def readme
+            template 'README.md'
           end
         end
       end
---
+++ lib/sauce/recipes/rails/application/templates/README.md
@@ -0,0 +1,4 @@
+<%= @name.capitalize %>
+<%= (1..@name.length).to_a.map { '=' }.join %>
+
+App description
